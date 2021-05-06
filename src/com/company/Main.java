c1edfabfbd7116f03278c68c018361ba7db8c05b
weronika przymuszala <przymuszalaweronika@gmail.com>
2021-05-06 09:28:30 +0200

    Ciąg Fibonacciego

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,13 @@ public class Main {
         int z = 1;
         int y = 1;
         int wynik = 0;
-        int temp = 0;
         if (x > 1) {
             for (int i = 3; i <= x; i++) {
                 wynik = y + z;
                 y = z;
                 z = wynik;
             }
-            System.out.println("Wynik to:" + wynik);
+            System.out.println("Wynik to: " + wynik);
         } else {
             wynik = 1;
             System.out.println("Wynik to: " + wynik);
