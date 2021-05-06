53a6a0bd79c74c4f4517e75cedfaef5d60e8218f
weronika przymuszala <przymuszalaweronika@gmail.com>
2021-05-06 09:27:25 +0200

    Ciąg Fibonacciego

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Podaj liczbę: ");
+        System.out.println("Podaj liczbę: ");
         Scanner liczba = new Scanner(System.in);
         int x = liczba.nextInt();
         int z = 1;
-        int y=1;
+        int y = 1;
         int wynik = 0;
         int temp = 0;
-        if(x>1) {
-            for (int i = 3; i <= x; i++)
-            {
-wynik = y + z;
-y =z ;
-z = wynik;
-
-
+        if (x > 1) {
+            for (int i = 3; i <= x; i++) {
+                wynik = y + z;
+                y = z;
+                z = wynik;
             }
-            System.out.println("wynik to:" +   wynik );
-        }
-        else
-        {
+            System.out.println("Wynik to:" + wynik);
+        } else {
             wynik = 1;
             System.out.println("Wynik to: " + wynik);
         }
